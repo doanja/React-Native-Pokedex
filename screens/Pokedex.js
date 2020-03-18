5ba58adc6966531a75d2b70d0dd4e5567a90e66f
John Doan <doanja@plu.edu>
2020-03-18 15:15:09 -0700

    fixed issue where changing offset in pokedex would not scroll flatlist to the top

--- screens/Pokedex.js
+++ screens/Pokedex.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, FlatList, TouchableOpacity, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 import { MaterialIcons } from '@expo/vector-icons';
 import API from '../services/pokemonAPI';
@@ -8,6 +8,7 @@ import PokemonCard from '../components/PokemonCard';
 export default function Pokedex({ navigation }) {
   const [pokemonList, setPokemonList] = useState([]);
   const [offset, setOffset] = useState(0);
+  const flatListRef = useRef();
 
   useEffect(() => {
     API.getPokemonList(offset)
@@ -22,35 +23,47 @@ export default function Pokedex({ navigation }) {
   return (
     <View style={globalStyles.container}>
       <FlatList
+        ListFooterComponent={() => (
+          <View style={styles.icons}>
+            <TouchableOpacity
+              onPress={() => {
+                if (offset > 0) {
+                  setOffset(offset - 20);
+                  flatListRef.current.scrollToIndex({ index: 0, animated: true });
+                }
+              }}>
+              <MaterialIcons name='chevron-left' size={24} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setOffset(offset + 20);
+                flatListRef.current.scrollToIndex({ index: 0, animated: true });
+              }}>
+              <MaterialIcons name='chevron-right' size={24} />
+            </TouchableOpacity>
+          </View>
+        )}
         data={pokemonList}
         keyExtractor={item => item.name}
         numColumns={2}
+        ref={flatListRef}
         renderItem={({ item }) => (
           <PokemonCard
             name={item.name}
             url={item.url}
-            onPress={() => navigation.navigate('Pokemon', item)}
+            onPress={() => {
+              navigation.navigate('Pokemon', item);
+            }}
           />
         )}
       />
-      <View style={styles.icons}>
-        <TouchableOpacity
-          onPress={() => {
-            offset <= 0 ? null : setOffset(offset - 20);
-          }}>
-          <MaterialIcons name='chevron-left' size={24} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => setOffset(offset + 20)}>
-          <MaterialIcons name='chevron-right' size={24} />
-        </TouchableOpacity>
-      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   icons: {
-    paddingVertical: 15,
+    // paddingVertical: 15,
     paddingHorizontal: 10,
     flexDirection: 'row',
     justifyContent: 'space-between'
