980e31aa1ed4a521bf98f8145fd852b8b9639219
John Doan <doanja@plu.edu>
2020-03-25 14:39:20 -0700

    wip: displaying evolution method

--- components/Pokemon/Evolutions.js
+++ components/Pokemon/Evolutions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Dimensions } from 'react-native';
 import { globalStyles } from '../../styles/global';
 import { Card, CardHeader } from '../../components';
 
@@ -9,19 +9,44 @@ export default function Evolutions({ evolutions }) {
       <CardHeader>
         <Text style={globalStyles.headerText}>Evolutions</Text>
       </CardHeader>
-      {/* {evolutions.length > 0 ? (
-        evolutions.map(evolution => (
-          <View key={evolution.name}>
-            <TouchableOpacity>
-              <Text style={globalStyles.cardItem}>{evolution.name}</Text>
-            </TouchableOpacity>
-          </View>
-        ))
-      ) : (
-        <View>
-          <Text style={globalStyles.cardText}>None</Text>
+      {evolutions.map(evolution => (
+        <View key={evolution.name}>
+          <TouchableOpacity style={styles.imageContainer}>
+            <Image
+              style={styles.image}
+              source={{
+                uri: evolution.sprite
+              }}
+            />
+          </TouchableOpacity>
+          {/* {evolution.method.length === 0
+            ? null
+            : evolution.method.map(method => {
+                return (
+                  <Text key={method.name}>{`${method.name}: ${
+                    typeof method.condition === 'object' ? method.condition.name : method.condition
+                  }`}</Text>
+                );
+              })} */}
         </View>
-      )} */}
+      ))}
     </Card>
   );
 }
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: '50%',
+    borderWidth: 0.5,
+    borderColor: 'crimson',
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10
+  },
+  image: {
+    width: '50%',
+    height: Dimensions.get('window').width / 4,
+    alignSelf: 'center',
+    justifyContent: 'center'
+  }
+});
