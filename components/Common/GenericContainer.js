8b1f3e0bf838de232fbf681dd1f3574b40696343
John Doan <doanja@plu.edu>
2020-04-13 15:20:14 -0700

    removed spinenrs from common components

--- components/Common/GenericContainer.js
+++ components/Common/GenericContainer.js
@@ -3,7 +3,6 @@ import { Text } from 'react-native';
 import { globalStyles } from '../../styles/global';
 import { Card, CardHeader } from '..';
 import Species from './Species';
-import Spinner from '../images/Spinner';
 
 export default function GenericContainer({ name, species }) {
   return (
@@ -12,7 +11,7 @@ export default function GenericContainer({ name, species }) {
         <Text style={globalStyles.headerText}>{name}</Text>
       </CardHeader>
 
-      {species ? <Species species={species} /> : <Spinner />}
+      <Species species={species} />
     </Card>
   );
 }
