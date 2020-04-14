d32be93e07ff8126074687e6d77dbb21de967fd8
John Doan <doanja@plu.edu>
2020-04-14 13:51:53 -0700

    added custom styles to sub card component, updated move container component

--- components/Common/SubCard.js
+++ components/Common/SubCard.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from '../../styles/global';
 import { Card, CardHeader } from '../../components';
 
-export default function SubCard({ header, data }) {
+export default function SubCard({ header, data, capitalize }) {
+  const headerStyle = [globalStyles.headerText];
+  const bodyStyle = [globalStyles.cardText, capitalize && styles.capitalize];
+
   return (
     <Card>
       <CardHeader>
-        <Text style={globalStyles.headerText}>{header}</Text>
+        <Text style={headerStyle}>{header}</Text>
       </CardHeader>
 
       <View>
-        <Text style={globalStyles.cardText}>{!data ? '-' : data}</Text>
+        <Text style={bodyStyle}>{!data ? '-' : data}</Text>
       </View>
     </Card>
   );
 }
+
+const styles = StyleSheet.create({
+  capitalize: {
+    textTransform: 'capitalize',
+  },
+});
--- components/Move/MoveContainer.js
+++ components/Move/MoveContainer.js
@@ -15,7 +15,7 @@ export default function MoveContainer({ moveName, moveData }) {
 
       <MoveTypes types={moveData.type} />
 
-      {/* <SubCard header={'description'} data={moveData.description} />
+      <SubCard header={'description'} data={moveData.description} />
 
       <SubCard header={'power'} data={moveData.power} />
 
@@ -25,15 +25,37 @@ export default function MoveContainer({ moveName, moveData }) {
 
       <SubCard header={'priority'} data={moveData.priority} />
 
-      <SubCard header={'damage type'} data={moveData.damageType} />
+      <SubCard header={'damage type'} data={moveData.damageType} capitalize />
 
-      <SubCard header={'target'} data={moveData.target} />
+      <SubCard header={'target'} data={moveData.target} capitalize />
 
-      <SubCard header={'effect chance'} data={moveData.effectChance} /> */}
+      <SubCard header={'effect chance'} data={moveData.effectChance} />
 
-      {/* <MultipleItemSubCard header={'stat changes'} data={moveData.statChanges} /> */}
+      <MultipleItemSubCard header={'stat changes'} data={moveData.statChanges} />
 
-      {/* <SubCard header={'stat chance'} data={moveData.statChance} /> */}
+      <SubCard header={'stat chance'} data={moveData.statChance} />
+
+      <SubCard header={'drain amount'} data={moveData.drain} />
+
+      <SubCard header={'flinch rate'} data={moveData.flinchChance} />
+
+      <SubCard header={'healing amount'} data={moveData.healing} />
+
+      <SubCard header={'ailment'} data={moveData.ailment} capitalize />
+
+      <SubCard header={'ailment chance'} data={moveData.ailmentChance} />
+
+      <SubCard header={'category'} data={moveData.category} capitalize />
+
+      <SubCard header={'critical rate'} data={moveData.critRate} />
+
+      <SubCard header={'min hits'} data={moveData.minHits} />
+
+      <SubCard header={'max hits'} data={moveData.maxHits} />
+
+      <SubCard header={'min turns'} data={moveData.minTurns} />
+
+      <SubCard header={'max turns'} data={moveData.maxTurns} />
     </Card>
   );
 }
