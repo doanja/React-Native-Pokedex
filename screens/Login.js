28998f0b8d465367c978338f469db486d2a5a6ea
John Doan <doanja@plu.edu>
2020-04-20 15:56:36 -0700

    created simple login form

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
+import LoginContainer from '../components/Login/LoginContainer';
+// import API from '../services/pokemonAPI';
 
 export default function Login() {
   return (
     <View style={globalStyles.container}>
-      <Text>Login Screen</Text>
+      <LoginContainer />
     </View>
   );
 }
--- styles/global.js
+++ styles/global.js
@@ -4,35 +4,36 @@ import { colors, sizes } from '../constants/theme';
 export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: sizes.padding
+    padding: sizes.padding,
   },
   titleText: {
     textTransform: 'capitalize',
     fontSize: 14,
-    color: '#333'
+    color: '#333',
   },
   paragraph: {
     marginVertical: 8,
     lineHeight: sizes.base,
-    color: 'black'
+    color: 'black',
   },
   input: {
-    borderWidth: 1,
+    borderWidth: 0.5,
     borderColor: colors.primary,
     padding: 10,
     fontSize: 18,
-    borderRadius: 6
+    borderRadius: 6,
+    marginBottom: 10,
   },
   errorText: {
     color: colors.primary,
     fontWeight: 'bold',
     marginBottom: 10,
     marginTop: 6,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   headerText: {
     color: colors.white,
-    textTransform: 'capitalize'
+    textTransform: 'capitalize',
   },
   cardItem: {
     color: colors.black,
@@ -42,12 +43,12 @@ export const globalStyles = StyleSheet.create({
     margin: 3,
     padding: 10,
     textAlign: 'center',
-    textTransform: 'capitalize'
+    textTransform: 'capitalize',
   },
   cardText: {
     color: colors.black,
     margin: 3,
     padding: 10,
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
 });
